2cf819b8453686bc46ef07621ecc1d94cf0f87f2
Vladislav_Poroshin <vld.poroshin@gmail.com>
2023-05-12 18:35:06 +0400

    Added __init__ to class Item and test for him.

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+"""Тесты модуля item.py."""
+
+
+@pytest.fixture
+def get_example():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_item(get_example):
+    """
+    TestCase Item
+    """
+    assert get_example.name == 'Смартфон'
+    assert get_example.price == 10000
+    assert get_example.quantity == 20
+
