6faf5e03e0f6c94512717c4f900bc233fce5cd81
Vladislav_Poroshin <vld.poroshin@gmail.com>
2023-05-16 14:23:43 +0400

    Added __repr__, __str__ method to class Item  and test for him.

--- src/item.py
+++ src/item.py
@@ -24,7 +24,10 @@ class Item:
         Item.all.append(self)
 
     def __repr__(self):
-        return f'\nself.name={self.name}\nself.prise={self.price}\nself.quantity={self.quantity}\n'
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return f'{self.__name}'
 
     def calculate_total_price(self) -> float:
         """
