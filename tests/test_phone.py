4a09baa1d0c6a1c6fb66385d5867ece7503070d8
Vladislav_Poroshin <vld.poroshin@gmail.com>
2023-05-23 17:23:04 +0400

    Created a new Phone class and tests for it.

---
+++ tests/test_phone.py
@@ -0,0 +1,39 @@
+import pytest
+from src.phone import Phone
+
+
+@pytest.fixture
+def phone_exmpl():
+    """фикстура возвращает список из двух экземпляров класса Phone"""
+    inst = Phone('Samsung', 20000, 15, 1)
+    inst2 = Phone('Xiaomi', 12000, 35, 2)
+    return [inst, inst2]
+
+
+def test_phone(phone_exmpl):
+    """Тест инициализации класса Phone"""
+    assert phone_exmpl[0].name == 'Samsung'
+    assert phone_exmpl[0].price == 20000
+    assert phone_exmpl[0].quantity == 15
+    assert phone_exmpl[0].number_of_sim == 1
+    assert phone_exmpl[1].name == 'Xiaomi'
+    assert phone_exmpl[1].price == 12000
+    assert phone_exmpl[1].quantity == 35
+    assert phone_exmpl[1].number_of_sim == 2
+
+
+def test_repr(phone_exmpl):
+    """Тест __repr__"""
+    assert repr(phone_exmpl[0]) == "Phone('Samsung', 20000, 15, 1)"
+
+
+def test_number_of_sim(phone_exmpl):
+    """Тест setter number_of_sim"""
+    phone_exmpl[1].number_of_sim = 1
+    assert phone_exmpl[1].number_of_sim == 1
+
+    try:
+        phone_exmpl[1].number_of_sim = 0
+
+    except ValueError:
+        assert phone_exmpl[1].number_of_sim == 1
